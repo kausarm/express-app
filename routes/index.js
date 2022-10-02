512c891bed7ba4e06082308f98e5fffa34130e97
kausarm <muhammadkausar01@gmail.com>
2022-10-02 11:07:13 +0700

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json(
-      "Welcome to my API"
+     { message:"Welcome to 1957301007 API"}
   );
 });
 
