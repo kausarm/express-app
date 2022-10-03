d8e8dfa4b19a7ecbece3938b71a552c0ee69ac0b
kausarm <muhammadkausar01@gmail.com>
2022-10-03 16:46:28 +0700

    rm

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ var createError = require('http-errors');
 var express = require('express');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
