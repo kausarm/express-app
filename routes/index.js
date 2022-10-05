a4303ed1c5e13821d49f22d4290a516f93d99cc0
kausarm <muhammadkausar01@gmail.com>
2022-10-05 16:54:34 +0700

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json(
-     { message:"Welcome to 1957301007 API"}
+  res.status(200).json(
+    { 
+      nama:"Muhammad Kausar",
+      nim:"1957301007"
+    }
   );
 });
 
